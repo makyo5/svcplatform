647114a5bc5f9128423da0a67b9332b0c47ad931
syl-work-pc <2581028207@qq.com>
2019-08-13 15:47:04 +0800

    continu

--- README.md
+++ README.md
@@ -11,8 +11,13 @@ semantic-ui-css
 
 antd(暂时未用)
 
+@antv/data-set
+
 react-router
 react-router-dom
 
 redux
 react-redux
+
+alasql
+moment
--- src/Component/XxkComponent/WeChartComponent.js
+++ src/Component/XxkComponent/WeChartComponent.js
@@ -2,7 +2,9 @@ import React, { Component } from "react";
 import { Grid } from "semantic-ui-react";
 import { G2, Chart, Geom, Axis, Tooltip, View } from "bizcharts";
 import Slider from "bizcharts-plugin-slider";
+
 import * as al from "alasql";
+import * as moment from "moment";
 
 import * as ax from "axios";
 import * as DataSet from "@antv/data-set";
@@ -11,21 +13,13 @@ const ApiUrl = {
     "WeChat": "http://10.101.2.2:50500/api/clinic/ClinicWeChatRegistryPayment"
 }
 
-const cols = {
-    value: {
-        min: 0
-    },
-    VISIT_DATE: {
-        range: [0, 1]
-    }
-};
-
 class WeChatComponent extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            WeChatClinicData: []
+            WeChatClinicData: [],
+            Frequency:""
         }
     }
     componentDidMount() {
@@ -34,8 +28,9 @@ class WeChatComponent extends Component {
         ax.get(ApiUrl.WeChat)
             .then((res) => {
                 
-                const ress = al("select * from ? where VISIT_DATE>'2019-06-01'", [res.data]);
+                const ress = al("select * from ? where YEAR='2019' ORDER BY WEEK ASC", [res.data]);
                 console.log(ress);
+                console.log(moment());
 
                 this.setState({
                     WeChatClinicData: ress
@@ -46,24 +41,83 @@ class WeChatComponent extends Component {
             })
     }
 
-    render() {        
-
-        return (
-            <div>
-                <Chart height={400} data={this.state.WeChatClinicData} forceFit >
-                    <Axis name="VISIT_DATE" label={{ rotate: 60 }} />
-                    <Axis name="OFF_LINE_REG_ACCOUNT" />
-                    <Geom type="line" position="VISIT_DATE*OFF_LINE_REG_ACCOUNT" size={2} color={"#02db46"} />
-                    <Geom type="line" position="VISIT_DATE*ON_LINE_REG_ACCOUNT" size={2} color={"#f586a0"}/>
-                    
-                    <Tooltip
-                        crosshairs={{
-                        type:"y"
-                    }}
-                    />
-                </Chart>
-            </div>
-        )
+    render() {
+        if (this.state.WeChatClinicData==[]) {
+            return (
+                <div>Loading...</div>
+            )
+        }
+        else {
+            return (
+                <Grid columns={2}>
+                    <Grid.Row>
+                        <Grid.Column>
+                        <div>挂号数量</div>
+                        <Chart height={200} data={this.state.WeChatClinicData} forceFit >
+                            <Axis name="WEEK" label={{
+                                rotate: 60
+                            }} />                    
+                            <Geom type="line" position="WEEK*OFF_LINE_REG_ACCOUNT" size={2} color={"#2fb9f5"} />
+                            <Geom type="line" position="WEEK*ON_LINE_REG_ACCOUNT" size={2} color={"#f687a1"} />
+                            <Tooltip
+                                crosshairs={{
+                                type:"y"
+                            }}
+                            />
+                        </Chart>
+                        </Grid.Column>
+                        <Grid.Column>
+                        <div>缴费数量</div>
+                        <Chart height={200} data={this.state.WeChatClinicData} forceFit >
+                            <Axis name="WEEK" label={{
+                                rotate: 60
+                            }} />                    
+                            <Geom type="line" position="WEEK*OFF_LINE_FEE_ACCOUNT" size={2} color={"#2fb9f5"} />
+                            <Geom type="line" position="WEEK*ON_LINE_FEE_ACCOUNT" size={2} color={"#f687a1"} />
+                            <Tooltip
+                                crosshairs={{
+                                type:"y"
+                            }}
+                            />
+                        </Chart>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column>
+                        <div>挂号金额</div>
+                        <Chart height={200} data={this.state.WeChatClinicData} forceFit >
+                            <Axis name="WEEK" label={{
+                                rotate: 60
+                            }} />                    
+                            <Geom type="line" position="WEEK*OFF_LINE_REG_CHARGE" size={2} color={"#2fb9f5"} />
+                            <Geom type="line" position="WEEK*ON_LINE_REG_CHARGE" size={2} color={"#f687a1"} />
+                            <Tooltip
+                                crosshairs={{
+                                type:"y"
+                            }}
+                            />
+                        </Chart>
+                        </Grid.Column>
+                        <Grid.Column>
+                        <div>缴费金额</div>
+                        <Chart height={200} data={this.state.WeChatClinicData} forceFit >
+                            <Axis name="WEEK" label={{
+                                rotate: 60
+                            }} />                    
+                            <Geom type="line" position="WEEK*OFF_LINE_FEE_CHARGE" size={2} color={"#2fb9f5"} />
+                            <Geom type="line" position="WEEK*ON_LINE_FEE_CHARGE" size={2} color={"#f687a1"} />
+                            <Tooltip
+                                crosshairs={{
+                                type:"y"
+                            }}
+                            />
+                        </Chart>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            )
+        }
+        
     }
 }
 export default WeChatComponent;
